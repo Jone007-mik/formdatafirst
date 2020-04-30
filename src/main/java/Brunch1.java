da5960c91b5a28528c0dcfad0f4ba121b7a3d426
Jone007-mik <lcj8633@163.com>
2020-04-30 10:34:58 +0800

    4.30——second10.34

--- src/main/java/Brunch1.java
+++ src/main/java/Brunch1.java
@@ -1,5 +1,7 @@
 public class Brunch1{
     public static void main(String[] args) {
         System.out.println("nihao!!!");
+        System.out.println("nihao!!!");
+        System.out.println("nihao!!!");
     }
 }
