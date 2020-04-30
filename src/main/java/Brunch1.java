bd04f84bc81307087264a8f13efea8bbe259648e
Jone007-mik <lcj8633@163.com>
2020-04-30 11:28:35 +0800

    4.30——secondbranch1是99

--- src/main/java/Brunch1.java
+++ src/main/java/Brunch1.java
@@ -1,9 +1,7 @@
 public class Brunch1{
     public static void main(String[] args) {
 
-        System.out.println("branch1是主分支");
-        System.out.println("loren ");
-        System.out.println("loren ");
-        System.out.println("loren ");
+        System.out.println("111主分支");
+
     }
 }
