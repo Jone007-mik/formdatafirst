2dea206467f1f92f2b77bef0492717e0d647c2f9
Jone007-mik <lcj8633@163.com>
2020-04-30 11:08:49 +0800

    4.30——secondbranch1是66

--- src/main/java/Brunch1.java
+++ src/main/java/Brunch1.java
@@ -1,8 +1,6 @@
 public class Brunch1{
     public static void main(String[] args) {
-        System.out.println("nihao!!!");
-        System.out.println("nihao!");
-        System.out.println("nihao!");
+
         System.out.println("branch1是主分支");
         System.out.println("loren ");
     }
